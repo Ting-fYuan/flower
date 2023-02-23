a2cfb7c500ed4c8ecfe2d0c92f107290593a0649
Keyleaf <547377191@qq.com>
2023-02-23 16:05:35 +0800

    更新 16:05

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,12 +1,42 @@
 <!-- 购物车 -->
 <template>
-  <div>购物车</div>
+  <div class="shop-view-box">
+    <com-head title="购物车"></com-head>
+    <main>
+      <div class="shop-car-box">
+        <div class="shop" v-if="showShopList">有购物车</div>
+        <div class="no-shop" v-else>没有购物车</div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
 export default {
   name: "ShopView",
+  data() {
+    return {
+      showShopList: false,
+    };
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.shop-view-box {
+  background-color: #e8ecef;
+  height: 100vh;
+  main {
+    padding: 42px 0 0;
+    .shop-car-box {
+      .shop {
+      }
+      .no-shop {
+        width: 100vw;
+        min-height: 260px;
+        background: #ffffff;
+      }
+    }
+  }
+}
+</style>
