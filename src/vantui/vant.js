4f3d1e9aac898e782f910396e9b83e6bfe9f1622
ilu <1255124824@qq.com>
2023-02-23 13:06:34 +0800

    123

--- src/vantui/vant.js
+++ src/vantui/vant.js
@@ -1,5 +1,11 @@
 import Vue from "vue";
-import { AddressList, Toast } from "vant";
+import { AddressList, Toast, Form, Field, Button } from "vant";
 
 Vue.use(AddressList);
 Vue.use(Toast);
+// 表单验证
+Vue.use(Form);
+// 输入框
+Vue.use(Field);
+// 按钮
+Vue.use(Button);
--- src/views/user_infomation/Login.vue
+++ src/views/user_infomation/Login.vue
@@ -1,12 +1,85 @@
 <!-- 登录 -->
 <template>
-  <div>登录</div>
+  <div class="wrap">
+    <header>
+      <div class="login_head">
+        <div class="head_back"><i class="iconfont icon-zuojiantou"></i></div>
+        <div class="head_text">登录注册</div>
+        <div class="head_option"><i class="iconfont icon-caidan"></i></div>
+      </div>
+      <div class="logo"></div>
+    </header>
+    <main>
+      <van-form @submit="onSubmit">
+        <van-field
+          v-model="username"
+          name="账号"
+          label="账号"
+          placeholder="请输入账号"
+          :rules="[{ required: true, message: '请填写用户名' }]"
+        />
+        <van-field
+          v-model="password"
+          type="password"
+          name="密码"
+          label="密码"
+          placeholder="请输入密码"
+          :rules="[{ required: true, message: '请填写密码' }]"
+        />
+        <div style="margin: 16px">
+          <van-button round block type="info" native-type="submit"
+            >提交</van-button
+          >
+        </div>
+      </van-form>
+    </main>
+    <footer></footer>
+  </div>
 </template>
 
 <script>
 export default {
   name: "LoginView",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    onSubmit(values) {
+      console.log("submit", values);
+    },
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.wrap {
+  header {
+    .login_head {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      i {
+        font-size: 18px;
+      }
+      .head_back {
+      }
+      .head_text {
+        font-size: 16px;
+        line-height: 40px;
+      }
+      .head_option {
+      }
+    }
+    .logo {
+      margin: 150px 0;
+    }
+  }
+  main {
+  }
+  footer {
+  }
+}
+</style>
