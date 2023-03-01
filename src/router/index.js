a7228c3e604bebe704421b6f03b4cd94de8787b8
Kayson <2431309107@qq.com>
2023-03-01 15:39:38 +0800

    feat:更新子页面的内容

--- src/router/index.js
+++ src/router/index.js
@@ -270,6 +270,14 @@ VueRouter.prototype.push = function push(location, onResolve, onReject) {
   return originalPush.call(this, location).catch((err) => err);
 };
 
+const originalReplace = VueRouter.prototype.replace;
+VueRouter.prototype.replace = function replace(location, onResolve, onReject) {
+  if (onResolve || onReject) {
+    return originalReplace.call(this, location, onResolve, onReject);
+  }
+  return originalReplace.call(this, location).catch((err) => err);
+};
+
 const router = new VueRouter({
   mode: "hash",
   base: process.env.BASE_URL,
